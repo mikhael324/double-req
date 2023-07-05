cc438fb5a5d0414d5f68f9e8fe9af909af393735
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2023-07-05 12:26:54 +0530

    Better result

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -84,6 +84,9 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
             else:
                 max_results = int(MAX_B_TN)
     query = query.strip()
+     if filter:
+        query = query.replace(' ', r'.*[\s\.\+\-_\(\)]')
+        raw_pattern = r'((\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_]))'
     #if filter:
         #better ?
         #query = query.replace(' ', r'(\s|\.|\+|\-|_)')
@@ -91,9 +94,9 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
     if not query:
         raw_pattern = '.'
     elif ' ' not in query:
-        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+        raw_pattern = r'((\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_]))'
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_\(\)]')
     
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
