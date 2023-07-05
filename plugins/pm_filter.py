15a51de076c5cdb68a5fe4bdedc688129fdd85cc
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2023-07-05 17:08:43 +0530

    Idk 😐

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -274,7 +274,7 @@ async def advantage_spoll_choker(bot, query):
             reqstr = await bot.get_users(reqstr1)
             if NO_RESULTS_MSG:
                 await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
-            k = await query.message.edit(f"👋 Hello {query.from_user.mention},\n\n <b>I couldn't find </b><b>'{search}'</b><b> in my Database</b> \n \n <b> Maybe Not Yet Released In OTT Platforms ⚠️</b>")
+            k = await query.message.edit(f"👋 Hello {reqstr.mention},\n\n <b>I couldn't find </b><b>'{movie}'</b><b> in my Database</b> \n \n <b> Maybe Not Yet Released In OTT Platforms ⚠️</b>")
             await asyncio.sleep(60)
             await k.delete()
             try:
