5211e489b83c03be7aec4ef57328f5cf1b1b661b
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2023-07-05 22:00:27 +0530

    Ccl

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1401,7 +1401,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b>Hᴇʏ {message.from_user.mention}, Hᴇʀᴇ ɪs Wʜᴀᴛ I Fᴏᴜɴᴅ Iɴ Mʏ Dᴀᴛᴀʙᴀsᴇ Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {search}.</b>"
+        cap = f"<b>┏ 🤴 ᴀsᴋᴇᴅ ʙʏ : {message.from_user.mention} \n┗ 🧡 ᴊᴏɪɴ : { @cinemaclub_3} \n \n🌩 ᴘᴏᴡᴇʀᴇᴅ ʙʏ \n{ @cinemaclub_4}</b>\n \n \n<b>Hᴇʀᴇ ɪs Wʜᴀᴛ I Fᴏᴜɴᴅ Iɴ Mʏ Dᴀᴛᴀʙᴀsᴇ Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {search}.</b>"
     if imdb and imdb.get('poster'):
         try:
             if message.chat.id == SUPPORT_CHAT_ID:
