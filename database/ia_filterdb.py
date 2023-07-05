2630ea75ffb7c23ea9096e1c2fbeeb115e6e6c33
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2023-07-05 12:31:22 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -84,7 +84,7 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
             else:
                 max_results = int(MAX_B_TN)
     query = query.strip()
-     if filter:
+    if filter:
         query = query.replace(' ', r'.*[\s\.\+\-_\(\)]')
         raw_pattern = r'((\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_]))'
     #if filter:
