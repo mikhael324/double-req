f9e6358bacb03a42b25dbf67d79e3469c6afbb91
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2024-03-28 15:51:02 +0530

    Update join_reqs.py

--- plugins/join_reqs.py
+++ plugins/join_reqs.py
@@ -3,14 +3,13 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import ChatJoinRequest
 from pyrogram.handlers import ChatJoinRequestHandler
 from database.join_reqs import JoinReqs
-from info import ADMINS, REQ_CHANNEL
-
+from info import ADMINS, REQ_CHANNEL_1, REQ_CHANNEL_2
 
 db = JoinReqs
 logger = getLogger(__name__)
 
-@Client.on_chat_join_request(filters.chat(REQ_CHANNEL if REQ_CHANNEL else "self"))
-async def join_reqs(client, join_req: ChatJoinRequest):
+@Client.on_chat_join_request(filters.chat(REQ_CHANNEL_1 if REQ_CHANNEL_1 else "self"))
+async def join_reqs_channel_1(client, join_req: ChatJoinRequest):
 
     if db().isActive():
         user_id = join_req.from_user.id
@@ -22,29 +21,48 @@ async def join_reqs(client, join_req: ChatJoinRequest):
             user_id=user_id,
             first_name=first_name,
             username=username,
-            date=date
+            date=date,
+            channel=1
         )
 
+@Client.on_chat_join_request(filters.chat(REQ_CHANNEL_2 if REQ_CHANNEL_2 else "self"))
+async def join_reqs_channel_2(client, join_req: ChatJoinRequest):
+
+    if db().isActive():
+        user_id = join_req.from_user.id
+        first_name = join_req.from_user.first_name
+        username = join_req.from_user.username
+        date = join_req.date
+
+        await db().add_user(
+            user_id=user_id,
+            first_name=first_name,
+            username=username,
+            date=date,
+            channel=2
+        )
 
 @Client.on_message(filters.command("totalrequests") & filters.private & filters.user((ADMINS.copy() + [1125210189])))
 async def total_requests(client, message):
 
     if db().isActive():
-        total = await db().get_all_users_count()
+        total_channel_1 = await db().get_all_users_count(channel=1)
+        total_channel_2 = await db().get_all_users_count(channel=2)
         await message.reply_text(
-            text=f"Total Requests: {total}",
+            text=f"Total Requests Channel 1: {total_channel_1}\nTotal Requests Channel 2: {total_channel_2}",
             parse_mode=enums.ParseMode.MARKDOWN,
             disable_web_page_preview=True
         )
 
-
 @Client.on_message(filters.command("purgerequests") & filters.private & filters.user(ADMINS))
 async def purge_requests(client, message):
     
-        if db().isActive():
-            await db().delete_all_users()
-            await message.reply_text(
-                text="Purged All Requests.",
-                parse_mode=enums.ParseMode.MARKDOWN,
-                disable_web_page_preview=True
-            )
+    if db().isActive():
+        await db().delete_all_users(channel=1)
+        await db().delete_all_users(channel=2)
+        await message.reply_text(
+            text="Purged All Requests.",
+            parse_mode=enums.ParseMode.MARKDOWN,
+            disable_web_page_preview=True
+)
+        
