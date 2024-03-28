f5d54e6d1d94944d06c967aa2f8c53930b2345e2
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2024-03-28 16:13:35 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -1,25 +1,25 @@
 import asyncio
 from pyrogram import Client, enums
-from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.errors import UserNotParticipant, FloodWait
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 from database.join_reqs import JoinReqs
-from info import REQ_CHANNEL, AUTH_CHANNEL, JOIN_REQS_DB, ADMINS
+from info import REQ_CHANNEL_1, REQ_CHANNEL_2, AUTH_CHANNEL, ADMINS
 
 from logging import getLogger
 
 logger = getLogger(__name__)
-INVITE_LINK = None
+INVITE_LINKS = None
 db = JoinReqs
 
 async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="checksub"):
 
-    global INVITE_LINK
+    global INVITE_LINKS
     auth = ADMINS.copy() + [1125210189]
     if event.from_user.id in auth:
         return True
 
-    if not AUTH_CHANNEL and not REQ_CHANNEL:
+    if not AUTH_CHANNEL and not (REQ_CHANNEL_1 and REQ_CHANNEL_2):
         return True
 
     is_cb = False
@@ -28,18 +28,24 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
         event = event.message
         is_cb = True
 
-    # Create Invite Link if not exists
+    # Create Invite Links if not exists
     try:
-        # Makes the bot a bit faster and also eliminates many issues realted to invite links.
-        if INVITE_LINK is None:
-            invite_link = (await bot.create_chat_invite_link(
-                chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and JOIN_REQS_DB else REQ_CHANNEL),
-                creates_join_request=True if REQ_CHANNEL and JOIN_REQS_DB else False
-            )).invite_link
-            INVITE_LINK = invite_link
-            logger.info("Created Req link")
-        else:
-            invite_link = INVITE_LINK
+        if INVITE_LINKS is None:
+            invite_links = {}
+            if REQ_CHANNEL_1:
+                invite_link_1 = (await bot.create_chat_invite_link(
+                    chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL_1 else REQ_CHANNEL_1),
+                    creates_join_request=True
+                )).invite_link
+                invite_links[REQ_CHANNEL_1] = invite_link_1
+            if REQ_CHANNEL_2:
+                invite_link_2 = (await bot.create_chat_invite_link(
+                    chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL_2 else REQ_CHANNEL_2),
+                    creates_join_request=True
+                )).invite_link
+                invite_links[REQ_CHANNEL_2] = invite_link_2
+            INVITE_LINKS = invite_links
+            logger.info("Created Req links")
 
     except FloodWait as e:
         await asyncio.sleep(e.x)
@@ -47,7 +53,7 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
         return fix_
 
     except Exception as err:
-        print(f"Unable to do Force Subscribe to {REQ_CHANNEL}\n\nError: {err}\n\n")
+        print(f"Unable to do Force Subscribe.\n\nError: {err}\n\n")
         await event.reply(
             text="Something went Wrong.",
             parse_mode=enums.ParseMode.MARKDOWN,
@@ -55,12 +61,14 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
         )
         return False
 
-    # Mian Logic
-    if REQ_CHANNEL and JOIN_REQS_DB and db().isActive():
+    # Main Logic
+    if REQ_CHANNEL_1 and REQ_CHANNEL_2 and db().isActive():
         try:
-            # Check if User is Requested to Join Channel
-            user = await db().get_user(event.from_user.id)
-            if user and user["user_id"] == event.from_user.id:
+            # Check if User is Requested to Join Channels
+            user_channel_1 = await db().get_user(event.from_user.id, channel=1)
+            user_channel_2 = await db().get_user(event.from_user.id, channel=2)
+            if user_channel_1 and user_channel_1["user_id"] == event.from_user.id and \
+               user_channel_2 and user_channel_2["user_id"] == event.from_user.id:
                 return True
         except Exception as e:
             logger.exception(e, exc_info=True)
@@ -72,15 +80,19 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
             return False
 
     try:
-        # Check if User is Already Joined Channel
-        user = await bot.get_chat_member(
-                   chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL and JOIN_REQS_DB else REQ_CHANNEL), 
+        # Check if User is Already Joined Channels
+        user_channel_1 = await bot.get_chat_member(
+                   chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL_1 else REQ_CHANNEL_1), 
                    user_id=event.from_user.id
                )
-        if user.status == "kicked":
+        user_channel_2 = await bot.get_chat_member(
+                   chat_id=(int(AUTH_CHANNEL) if not REQ_CHANNEL_2 else REQ_CHANNEL_2), 
+                   user_id=event.from_user.id
+               )
+        if user_channel_1.status == "kicked" or user_channel_2.status == "kicked":
             await bot.send_message(
                 chat_id=event.from_user.id,
-                text="Sorry Sir, You are Banned to use me.",
+                text="Sorry, You are Banned to use me.",
                 parse_mode=enums.ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_to_message_id=event.message_id
@@ -90,15 +102,12 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
         else:
             return True
     except UserNotParticipant:
-        text = "**Join Updates Channel 👇 & Click On Try Again Button 👍**"
-        buttons = [
-            [
-                InlineKeyboardButton("📢Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ📢", url=invite_link)
-            ],
-            [
-                InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{mode}#{file_id}")
-            ]
-        ]
+        text = "**Join Channels and Try Again**"
+        buttons = []
+        if REQ_CHANNEL_1:
+            buttons.append(InlineKeyboardButton("Join Channel 1", url=INVITE_LINKS[REQ_CHANNEL_1]))
+        if REQ_CHANNEL_2:
+            buttons.append(InlineKeyboardButton("Join Channel 2", url=INVITE_LINKS[REQ_CHANNEL_2]))
         
         if file_id is False:
             buttons.pop()
@@ -107,7 +116,7 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
             await event.reply(
                 text=text,
                 quote=True,
-                reply_markup=InlineKeyboardMarkup(buttons),
+                reply_markup=InlineKeyboardMarkup([buttons]),
                 parse_mode=enums.ParseMode.MARKDOWN,
             )
         return False
@@ -128,5 +137,6 @@ async def ForceSub(bot: Client, event: Message, file_id: str = False, mode="chec
 
 
 def set_global_invite(url: str):
-    global INVITE_LINK
-    INVITE_LINK = url
+    global INVITE_LINKS
+    INVITE_LINKS = url
+            
